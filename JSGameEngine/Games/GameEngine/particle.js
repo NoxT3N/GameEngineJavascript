66e0d1558df5d5911026eb2eff104affe3ac59b5
NoxT3N <145358483+NoxT3N@users.noreply.github.com>
2023-11-23 16:01:26 +0000

    Added Particle component

---
+++ JSGameEngine/Games/GameEngine/particle.js
@@ -0,0 +1,20 @@
+import GameObject from "../gameobject.js";
+import Renderer from "../renderer.js";
+import Physics from "../physics.js";
+
+class Particle extends GameObject{
+    constructor(x,y,width,height,color,duration){
+        super(x,y);
+        this.duration = duration;
+        this.addComponent(new Physics({x:0,y:0},{x:0,y:0}));
+        this.addComponent(new Renderer(color,width, height));
+    }
+    update(deltaTime){
+        this.duration -= deltaTime;
+        if(this.duration<=0){
+            this.game.removeGameObject(this)
+        }
+        super.update(deltaTime);
+    }
+}
+export default Particle;
