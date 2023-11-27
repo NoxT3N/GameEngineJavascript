49c91fbb36f7ef86ff069c7c503508980d3cd384
NoxT3N <145358483+NoxT3N@users.noreply.github.com>
2023-11-27 16:00:42 +0000

    added player

--- JSGameEngine/Game/platform.js
+++ JSGameEngine/Game/platform.js
@@ -2,3 +2,12 @@ import GameObject from "../GameEngine/gameobject";
 import Renderer from "../GameEngine/renderer";
 import Physics from "../GameEngine/physics";
 
+class Platform extends GameObject{
+    constructor(x,y,width,height,color = "gray"){
+        super(x,y);
+        this.addComponent(new Renderer(color,width,height));
+        this.addComponent(new Physics({x:0,y:0},{x:0,y:0},{x:0,y:0}));
+
+        this.tag = "platform";
+    }
+} export default Platform;
