660fc037b5048037d940ef9a3e059bad155472e6
NoxT3N <145358483+NoxT3N@users.noreply.github.com>
2023-11-27 14:43:47 +0000

    added particlesSystem

---
+++ JSGameEngine/Game/collectible.js
@@ -0,0 +1,7 @@
+import GameObject from "../GameEngine/gameobject";
+import Renderer from "../GameEngine/renderer";
+import Physics from "../GameEngine/physics";
+
+class Collectible extends GameObject{
+    
+}
